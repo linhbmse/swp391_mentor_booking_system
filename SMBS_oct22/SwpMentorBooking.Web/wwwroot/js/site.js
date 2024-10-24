29e5cbddfa7b7b7a96b10c88d283e02a2aad553e
Mai Hải Nam <nammhse184557@fpt.edu.vn>
2024-10-24 21:35:31 +0700

    Update View Mentor Schedule

--- SMBS_oct22/SwpMentorBooking.Web/wwwroot/js/site.js
+++ SMBS_oct22/SwpMentorBooking.Web/wwwroot/js/site.js
@@ -3,6 +3,32 @@
 
 // Write your JavaScript code.
 
+//function filterUser() {
+//    // Declare variables
+//    var input, filter, table, tr, td, i, txtValue;
+//    input = document.getElementById("searchInput");
+//    filter = input.value.toUpperCase();
+//    table = document.getElementById("userTable");
+//    tr = table.getElementsByTagName("tr");
+
+//    // Loop through all table rows, and hide those who don't match the search query
+//    for (i = 0; i < tr.length; i++) {
+//        td = tr[i].getElementsByTagName("td")[0];
+//        if (td) {
+//            txtValue = td.textContent || td.innerText;
+//            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//                tr[i].style.display = "";
+//            } else {
+//                tr[i].style.display = "none";
+//            }
+//        }
+//    }
+//}
+// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
 function filterUser() {
     // Declare variables
     var input, filter, table, tr, td, i, txtValue;
